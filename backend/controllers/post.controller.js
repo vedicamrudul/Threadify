013c49470830fc332f18d707c9b86cf5a3e53295
Vedica Mrudul <vedicaam80@gmail.com>
2025-01-11 17:10:09 +0530

    posts controller completed.

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,7 @@ const Post= require('../models/post.model');
 const User=require('../models/user.model');
 // crud- create, update, delete, get the posts. Apart from that we have comment on post, like/unlike post, get all posts by user, get all posts by following users, 
 
+// whats left- notification on liking the post, or commenting (follow unfollow is already implemented.)
 const {v2: cloudinary} = require('cloudinary');
 const {StatusCodes}=require('http-status-codes');
 
@@ -54,15 +55,18 @@ const commentOnPost= async (req, res)=>{
     const {text}=req.body;
     const userId=req.user._id;
 
-    const userExists=User.findById(userId);
-    if(!userExists) res.status(StatusCodes.BAD_REQUEST).json({message: "User does not exist"});
+    const userExists=await User.findById(userId);
+    if(!userExists) return res.status(StatusCodes.BAD_REQUEST).json({message: "User does not exist"});
 
     const post= await Post.findById(postId);
-    if(!post) res.status(StatusCodes.BAD_REQUEST).json({message: "Post does not exist"});
+    if(!post) return res.status(StatusCodes.BAD_REQUEST).json({message: "Post does not exist"});
 
-    if(!text) res.status(StatusCodes.BAD_REQUEST).json({message: "Please Enter Comment"});
+    if(!text) return res.status(StatusCodes.BAD_REQUEST).json({message: "Please Enter Comment"});
 
-    post.comments.push({text, postedBy: userId});
+    await Post.findByIdAndUpdate(postId, { $push: { comments: { text, postedBy: userId } } });
+
+
+    res.status(StatusCodes.OK).json({"msg": "commented successfully"})
     // we use push to add anything to an array in mongoose
 }
 
@@ -84,19 +88,105 @@ const likePost= async (req, res)=>{
    await User.findByIdAndUpdate(userId, {$push: {likedPosts: postId}});
 //    okay so i am not able to remember the syntax for updating array as its kinda new for me. So lets think about it this way. normally agar kuch update karna reheta hai jaise name so we would just pass the id and then the new name like ({id, name: "new name"}). 
 
-// ab when we are working with an array of OBJECTS which has multiple fields within it, we will obviously have to mention the field we are working with. Other than that we can want to perform various operations like push pull etc but normal mai aisa nai hota right? so we have to mention kauna operation perform karna hai. and to clarity ki its array (warna usko lagega field name hai right? ) we use $push or $. toh it basically becomes ({id, $push: {field: value}}). toh we want to push this field and value aisa. 
+// ab when we are working with an array of OBJECTS which has multiple fields within it, we will obviously have to mention the field we are working with. Other than that we can want to perform various operations like push pull etc but normal mai aisa nai hota right? so we have to mention kauna operation perform karna hai. and to clarity ki its array (warna usko lagega field name hai right? ) we use $push or $. toh it basically becomes (id, $push: {field: value}). toh we want to push this field and value aisa. 
     res.status(StatusCodes.OK).json({message: "Post Liked"});
    }
 
 }
+// lets finish getting posts of following people and get all the posts and get liked posts.
 
-const getAllPostsByUser= async (req, res)=>{
-    const UserId=req.user._id;
+const getFollowingPosts=async(req,res)=>{
+    const {userId}=req.params;
+
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    if(req.user._id != userId){ 
+        return res.status(StatusCodes.UNAUTHORIZED).json({"msg": "You are not authroized to get these posts"})}
+    
+    const following=user.following;
+    const result= await Post.find({postedBy: {$in: following}}).sort({createdBy: -1}).populate({path: "postedBy", select: "-password"}).populate({path: "comments.postedBy", select: "-password"});
+
+    res.status(StatusCodes.OK).json(result)
 }
+
+const getLikedPosts=async (req,res)=>{
+    const {userId}=req.params;
+    console.log("one");
+
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    console.log("two");
+
+    if(req.user._id != userId){ 
+        return res.status(StatusCodes.UNAUTHORIZED).json({"msg": "You are not authroized to get these posts"})}
+        console.log("three");
+
+    const result=await Post.find({'_id': {$in: user.likedPosts }})
+            .populate({
+                path: "postedBy",
+                select:"-password"
+            })
+            .populate({
+                path: "comments.postedBy",
+                select: "-password"
+            });
+    res.status(StatusCodes.OK).json(result)
+}
+
+const getAllPosts= async(req,res)=>{
+
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const result= await Post.find()
+    .populate({
+        path: "postedBy",
+        select: "-password"
+    })
+    .populate({
+        path: "comments.postedBy",
+        select: "-password"
+    })
+
+    res.status(StatusCodes.OK).json(result)
+}
+
+const getUserPosts = async (req, res) => {
+	try {
+		const { userId } = req.params;
+
+		const user = await User.findById(req.user._id);
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+        if(req.user._id != userId){ 
+            return res.status(StatusCodes.UNAUTHORIZED).json({"msg": "You are not authroized to get these posts"})}
+		const posts = await Post.find({ postedBy: userId })
+			.sort({ createdAt: -1 }).populate({path: 'comments.postedBy', select:'-password'})
+		
+
+            /*
+            POPULATE METHOD.
+            => normally .populate('user') karte hai ya .populate('user', 'username') but since idhar array hai of users (for comments in every post) plus we want to deselct the password we are using this syntax. .populate({ path: "we give the path isme the field is comments which an array of objects that refer to userid named postedBy. so we pass comments.postedBy and delselect the password field."})
+            */
+           console.log(posts)
+
+		res.status(200).json({user, posts });
+	} catch (error) {
+		console.log("Error in getUserPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
+
+
 
 module.exports={
     createPost,
     deletePost,
     commentOnPost,
-    likePost
+    likePost,
+    getUserPosts,
+    getFollowingPosts,
+    getAllPosts,
+    getLikedPosts
 }
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,16 @@
 const express=require('express');
 const router=express.Router();
 
-const {createPost,deletePost,commentOnPost}=require('../controllers/post.controller');
+const {createPost,deletePost,commentOnPost,getUserPosts, getLikedPosts, getAllPosts, getFollowingPosts, likePost}=require('../controllers/post.controller');
 const authMiddleware=require('../middlewear/authMiddlewear')
 
 router.post('/create',authMiddleware, createPost);
+router.post('/comment/:postId',authMiddleware, commentOnPost)
+router.post('/likepost/:postId',authMiddleware, likePost)
+router.get('/getuserposts/:userId', authMiddleware, getUserPosts)
+router.get('/getallposts',authMiddleware,getAllPosts)
+router.get('/getlikedposts/:userId',authMiddleware,getLikedPosts)
+router.get('/getfollowingposts/:userId',authMiddleware,getFollowingPosts)
 router.delete('/delete/:postId',authMiddleware, deletePost);
-router.post('/comment/:postId',authMiddleware, commentOnPost);
 
 module.exports=router;
